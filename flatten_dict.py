bdde141f0a15dadcaafba39d26f9472c78f7660e
mahesh-cr-de <umamahesh.ade@gmail.com>
2026-03-15 10:42:08 +0530

    Update flatten_dict.py

--- flatten_dict.py
+++ flatten_dict.py
@@ -1,1 +1,29 @@
+def flatten_dict(d, parent_key=""):
+    flattened = {}
 
+    for key, value in d.items():
+
+        new_key = f"{parent_key}.{key}" if parent_key else key
+
+        if isinstance(value, dict):
+            flattened.update(flatten_dict(value, new_key))
+        else:
+            flattened[new_key] = value
+
+    return flattened
+
+
+input_dict = {
+    "user": {
+        "id": 123,
+        "personal_info": {
+            "name": "Alex",
+            "email": "alex@example.com"
+        }
+    },
+    "status": "active"
+}
+
+result = flatten_dict(input_dict)
+
+print(result)
